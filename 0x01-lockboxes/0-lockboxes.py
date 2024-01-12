18d6a74c030e0bc15c995e2adf09888e6a6125b5
WIlliams <akanniwilliams@gmail.com>
2024-01-12 12:08:57 +0100

    Ignore key if box doesn't exist (do that for the second loop)

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,18 +11,21 @@ def canUnlockAll(boxes):
     """Check if all boxes can be unlocked"""
     if not isinstance(boxes, list) or boxes is None:
         return False
+
     total_boxes = len(boxes)
     keys = set([0])
     entered = {}
-    for idx, box in enumerate(boxes):
-        if idx in keys:
-            keys.update({x for x in box if x < total_boxes})
-            entered[idx] = True
-        else:
-            entered[idx] = False
-    for idx, val in entered.items():
-        if val is False and idx in keys:
-            keys.update(boxes[idx])
+
+    if total_boxes > 1:
+        for idx, box in enumerate(boxes):
+            if idx in keys:
+                keys.update({x for x in box if x < total_boxes})
+                entered[idx] = True
+            else:
+                entered[idx] = False
+        for idx, val in entered.items():
+            if val is False and idx in keys:
+                keys.update({x for x in boxes[idx] if x < total_boxes})
 
     if total_boxes == len(keys) or total_boxes == 0:
         return True
